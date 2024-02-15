fab53cb64f14bb503047001f9e6a267fd20c5c94
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-15 15:53:43 +0530

    buttons,player controls,play method,added playlist
    button for : play and pause ,
    css for button,
    method fro play and pause buttons

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,17 @@
       <h2 class="song-title">{{ current.title }}-
        <span>{{ current.artist }}</span>
       </h2>
+      <div id="control">
+        <button class="prev"> Prev. </button>
+        <button class="play" v-if="!isPlaying" @click="play">Play</button>
+        <button class="pause" v-else  @click="pause">Pause</button>
+        <button class="next">Next</button>
+      </div>
+    </section>
+    <section class="playlist">
+      <h3> The playlist</h3>
+      <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src)? 'song playing':'song '">{{ song.title }} - {{ song.artist }}</button>
+
     </section>
    </main>
   </div>
@@ -21,10 +32,9 @@ export default {
   name: 'App',
   data(){
     return{
-      current:{
-       
-      },
+      current:{},
       index:0,
+      isPlaying: false,
       songs:[
         {
           title: 'Greatful',
@@ -40,10 +50,25 @@ export default {
       player: new Audio()
     }
   },
+  methods:{
+    play(song)
+    {
+      if(typeof song.src !=='undefined'){
+        this.current=song;
+        this.player.src =this.current.src;
+      }
+      this.player.play();
+      this.isPlaying=true;
+   },
+   pause(){
+    this.player.pause();
+    this.isPlaying= false;
+   }  
+ },
   created(){
     this.current= this.songs[this.index];
     this.player.src = this.current.src;
-    this.player.play();
+    // this.player.play();
   }
 
 }
@@ -66,6 +91,46 @@ header{
   background-color: #212121;
   color: #FFF;
 }
-
-
+.prev {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  border-radius: 250px 0px 0px 250px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+.play , .pause {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  border-radius: 10px 10px 10px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+.next {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  border-radius: 0px 500px 500px 0px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
 </style>
