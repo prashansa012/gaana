ed3b42157846a8098aace85a7abb74bdaccb72e0
prashansa012 <156915156+prashansa012@users.noreply.github.com>
2024-02-15 14:46:20 +0530

    css update, linking of music audio to the file

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <template>
+  <div id="App">
+   <header>
+    <h1>My Music</h1>
+   </header>
+   <main>
+    <section class="player">
+      <h2 class="song-title">{{ current.title }}-
+       <span>{{ current.artist }}</span>
+      </h2>
+    </section>
+   </main>
+  </div>
   
 </template>
 
@@ -7,19 +19,53 @@
 
 export default {
   name: 'App',
-  components: {
-  
+  data(){
+    return{
+      current:{
+       
+      },
+      index:0,
+      songs:[
+        {
+          title: 'Greatful',
+          artist:'Neffex',
+          src: require('./assets/neffex-grateful.mp3')
+        },
+        {
+          title:'Invincible',
+          artist:'Deaf Kev',
+          src: require('./assets/deaf-kev-invincible.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+  created(){
+    this.current= this.songs[this.index];
+    this.player.src = this.current.src;
+    this.player.play();
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+body{
+  font-family: sans-serif;
+}
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #FFF;
+}
+
+
 </style>
